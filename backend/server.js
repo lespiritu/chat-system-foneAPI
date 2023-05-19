b97fa8f4b5a5e8756a07b413a20232028cd34a96
espirituleoncio7 <espirituleoncio7@gmail.com>
2023-05-19 22:47:55 +0800

    done with user login and sign up

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,31 @@
 const express = require('express');
-const connectDB = require('./config/db')
-const dotenv = require('dotenv')
-const { notFound, errorHandler } = require('./middleware/errorMiddleware') 
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
+dotenv.config();
 
-dotenv.config()
-
-const app = express()
-const PORT = process.env.PORT || 5000
-
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 // routes
-const userRoutes = require('./routes/userRoutes')
-connectDB()
+const userRoutes = require('./routes/userRoutes');
+connectDB();
 
 //middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 // app.use(cors());
 
-
-
 app.get('/', (req, res) => {
-    res.send("API is Running")
-})
+  res.send('API is Running');
+});
 
 // request routes
-app.use("/api/user", userRoutes);
+app.use('/api/user', userRoutes);
 
 //error handler
-app.use(notFound)
-app.use(errorHandler)
-
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(5000, console.log(`Server is running at port ${PORT}`));
