f7286bf91d53b3d2dd42719c348432b4e42ebf10
espirituleoncio7 <espirituleoncio7@gmail.com>
2023-05-22 05:47:18 +0800

    build added

--- backend/server.js
+++ backend/server.js
@@ -34,9 +34,9 @@ app.use('/api/message', messageRountes);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname1, '/frontend/build')));
+  app.use(express.static(path.join(__dirname1, '/build')));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname1, 'frontend', 'build', 'index.html')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname1, 'build', 'index.html')));
 } else {
   app.get('/', (req, res) => {
     res.send('API is running..');
